e8d45ec11835d074269311c6470b6c53c25f1d07
Gutierrez Rivera Marco Alexis <gutierrezrm2018@dci25-192.ugto.mx>
2018-08-08 09:25:54 -0500

    cree mi primer programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,13 @@
+/*Esto es un comentario. Me sirve para escribir informacion del programa.
+Imprime la frase "hola mundo" a la pantalla.gcc
+Creado el 06/08/2018 por Marco Gutiérrez*/
+
+#include<stdio.h>
+
+int main()
+	{
+		printf("hola mundo, que tal? \n");
+		printf("este es el primer programa del curso\n");
+	}	
+
+/*termine de escribir el programa*/
