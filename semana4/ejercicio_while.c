04f5cd8810f224cec75d865c15eb7e8e0ca1246e
Gutierrez Rivera Marco Alexis <gutierrezrm2018@dci25-192.ugto.mx>
2018-08-29 09:16:25 -0500

    semana 4

---
+++ semana4/ejercicio_while.c
@@ -0,0 +1,68 @@
+/*Este programa sirve para convertir temperaturas y coordenadas.
+No se sale del programa hasta que tu se lo indiques.
+Creado el 29/08/2018 por Marco Gutiérrez.*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int opcion;
+	float c, f, k; //c=celsius, f=fahrenheit, k=kelvin
+	float x, y; //P1(x,y)
+	float r, a; //P2(r,θ)
+
+	printf("Teclea 1 para seleccionar el convertidor de temperaturas.\n");
+	printf("Teclea 2 para seleccionar el convertidor de coordenadas.\n");
+	printf("teclea 3 para salir del programa\n");
+	scanf("%i", &opcion);
+
+	while(opcion==1 || opcion==2){
+
+	switch(opcion)
+	{
+	case 1:
+		printf("Dame una temperatura en °c:\n");
+		scanf("%f", &c);
+	
+		f=1.8*c+32;
+		k=c+273.15;
+
+		printf("Tu temperatura en celsius es: %f\n",c);
+		printf("Tu temperatura en fahrenheit es: %f\n",f);
+		printf("Tu temperatura en kelvin es: %f\n",k);
+		break;
+	case 2:
+		printf("Dame una coordenada cartesiana (x,y):\n");
+		scanf("%f", &x);
+		scanf("%f", &y);
+		printf("Tu coordenada cartesiana es: (%f,%f)\n", x, y);
+	
+		r= sqrt((pow(x,2))+(pow(y,2)));
+		a= (atan((y/x)))*(180/3.141592);
+	
+		printf("Tus coordenadas polares (r,θ) son: (%f,%f°)\n", r, a);
+
+		if(x>=0 && y>=0){
+			printf("Estas en el primer cuadrante: (+,+)\n");
+		}else{
+			if(x<0 && y>=0){
+				printf("Estas en el segundo cuadrante: (-,+)\n");
+			}else{
+				if(x<0 && y<0){
+					printf("Estas en el tercer cuadrante: (-,-)\n");
+				}else{
+					printf("Estas en el cuarto cuadrante: (+,-)\n");
+				}
+			}
+		}
+		break;
+	}
+
+	printf("Teclea 1 para seleccionar el convertidor de temperaturas.\n");
+	printf("Teclea 2 para seleccionar el convertidor de coordenadas.\n");
+	printf("teclea 3 para salir del programa.\n");
+	scanf("%i", &opcion);
+
+	}
+	return 0;
+}
