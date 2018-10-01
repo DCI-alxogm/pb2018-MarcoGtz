174809fe8d6333fec10c68ff152c2e58e0cff779
Gutierrez Rivera Marco Alexis <gutierrezrm2018@dci25-192.ugto.mx>
2018-10-01 12:11:41 -0500

    proyecto 1

--- Projecto1/proyecto.c
+++ Projecto1/proyecto.c
@@ -3,38 +3,74 @@
 
 int main()
 {
-    //Declarar variables
+    //Declacion de variables
 	FILE *fp;
 	int i;
 	float x[9],y[9],z[9],vx[9],vy[9],vz[9];
-	float xi[9],yi[9],zi[9],vxi[9],vyi[9],vzi[9];
-	float Msol,G,h,r;
+	float xi,yi,zi,vxi,vyi,vzi;
+	float Msol,G,r,j,f[9],h[9];
 	
-    //Escanear los datos
+    //Escaneo de datos
 	fp=fopen("Planetas.txt","r");
-	for(i=0;i<9;i++){
+	for(i=0;i<9;i++)
+	{
 	fscanf(fp,"%f %f %f %f %f %f",&x[i],&y[i],&z[i],&vx[i],&vy[i],&vz[i]);
+	fscanf(fp,"%f %f",&f[i],&h[i]);
 	}
 	fscanf(fp,"%f",&Msol);
-	fscanf(fp,"%f",&h);
 	fclose(fp);
 
-	//Operaciones y procesado de datos
+    //Operaciones e impresion de datos
 	G=4.0*3.1416*3.1416;
-	for(i=0;i<9;i++){
-	r=sqrt(pow(x[i],2)+pow(y[i],2)+pow(z[i],2));
-	xi[i]=x[i]+vx[i]*h;
-	yi[i]=y[i]+vy[i]*h;
-	zi[i]=z[i]+vz[i]*h;
-	vxi[i]=vx[i]-(h*G*Msol*x[i]/pow(r,3));
-	vyi[i]=vy[i]-(h*G*Msol*y[i]/pow(r,3));
-	vzi[i]=vz[i]-(h*G*Msol*z[i]/pow(r,3));
-	
+	for(i=0;i<9;i++)
+	{
+		switch(i)
+		{
+		case 0: fp=fopen("mercurio.txt","w");
+			break;
+		case 1: fp=fopen("venus.txt","w");
+			break;
+		case 2: fp=fopen("tierra.txt","w");
+			break;
+		case 3: fp=fopen("marte.txt","w");
+			break;
+		case 4: fp=fopen("jupiter.txt","w");
+			break;
+		case 5: fp=fopen("saturno.txt","w");
+			break;
+		case 6: fp=fopen("urano.txt","w");
+			break;
+		case 7: fp=fopen("neptuno.txt","w");
+			break;
+		case 8: fp=fopen("pluton.txt","w");
+			break;
+		}
+		
+		fprintf(fp,"Posición\t\tVelocidad");
+		fprintf(fp,"%f %f %f %f %f %f",x[i],y[i],z[i],vx[i],vy[i],vz[i]);
+
+		for(j=0;j<f;j+=h[i])
+		{
+		r=sqrt(pow(x[i],2)+pow(y[i],2)+pow(z[i],2));
+		xi=x[i]+vx[i]*h[i];
+		yi=y[i]+vy[i]*h[i];
+		zi=z[i]+vz[i]*h[i];
+		vxi=vx[i]-(h[i]*G*Msol*x[i]/pow(r,3));
+		vyi=vy[i]-(h[i]*G*Msol*y[i]/pow(r,3));
+		vzi=vz[i]-(h[i]*G*Msol*z[i]/pow(r,3));
+		
+		fprintf(fp,"%f %f %f %f %f %f",xi,yi,zi,vxi,vyi,vzi);
+
+		x[i]=xi;
+		y[i]=yi;
+		z[i]=zi;
+		vx[i]=vxi;
+		vy[i]=vyi;
+		vz[i]=vzi;
+		}
+
+		fclose(fp);
 	}
-	
-	//Impresion de los datos
-
-
 
 return 0;
 }
